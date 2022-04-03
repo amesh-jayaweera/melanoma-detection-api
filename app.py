314b2409174109377f84ae9d555aac27eb45c1c7
ameshmjayaweera <ameshmbjyw97@gmail.com>
2022-04-03 19:15:31 +0530

    [api] - uncommented melanoma_detection_by_dermoscopic_images

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from melanoma_detection_pps import detect_melanoma_by_pps, Data
+from melanoma_detection_dermoscopic_images import checkMelanoma
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/predict-melanoma/*": {"origins": "*"}})
@@ -21,21 +22,21 @@ def melanoma_detection_by_pps():
         return {"message": "Something went wrong!"}, 400  # bad request
 
 
-# @app.route('/predict-melanoma?byMethod=dermoscopic-images', methods=['POST'])
-# def melanoma_detection_by_dermoscopic_images():
-#     try:
-#         img = request.files['image']
-#         extension = img.filename.split(".")[1]
-#         if extension in required_file_extensions:
-#             output = checkMelanoma(img)
-#             response = jsonify(output)
-#             response.headers.add('Access-Control-Allow-Origin', '*')
-#             return response, 200  # request completed successfully
-#         else:
-#             return "Bad request", 400
-#     except Exception as e:
-#         print(e)
-#         return "Bad request", 400  # bad request
+@app.route('/predict-melanoma/dermoscopic-images', methods=['POST'])
+def melanoma_detection_by_dermoscopic_images():
+    try:
+        img = request.files['image']
+        extension = img.filename.split(".")[1]
+        if extension in required_file_extensions:
+            output = checkMelanoma(img)
+            response = jsonify(output)
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response, 200  # request completed successfully
+        else:
+            return "Bad request", 400
+    except Exception as e:
+        print(e)
+        return "Bad request", 400  # bad request
 
 
 if __name__ == "__main__":
